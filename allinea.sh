d814a096393a828fd920851a543c62106baeb3f9
caprowsky <alessandrocapra@gmail.com>
2019-05-27 10:49:06 +0200

    Aggiornamento dei files dopo prima verifica

--- allinea.sh
+++ allinea.sh
@@ -19,7 +19,7 @@ echo "Eseguo il git pull"
 #git pull origin master
 
 echo "faccio partire tutti i container in locale"
-./live.sh up
+./dev.sh up
 echo "Aspetto 1 minuto perché sto importando il db di produzione"
 sleep 60
 echo "Imposto wordpress per lo sviluppo locale"
@@ -27,7 +27,7 @@ echo "Imposto wordpress per lo sviluppo locale"
 # wp option update home 'http://dh.unica.localhost'
 # wp option update siteurl 'http://dh.unica.localhost'
 
-# wp search-replace --url=dh.unica.it dh.unica.it dh.unica.localhost 'wp_*options' wp_blogs
+wp search-replace --url=dh.unica.it dh.unica.it dh.unica.localhost 'wp_*options' wp_blogs
 
 
 echo "resetto la passowrd di admin in admin, mentre tutte le altre rimangono le stesse"
